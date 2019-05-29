ad726a31a8d875c1c0d5fd8f7fe9eaf1801c4768
cmattey <cmattey@dons.usfca.edu>
2019-05-28 20:10:48 -0700

    Changes to marker settings, and additional question list

--- main.py
+++ main.py
@@ -31,12 +31,15 @@ app.secret_key = 'dfasdfasdfasdf'
 
 
 class Page(object):
-    def __init__(self, chinese, answer, hint='', error_count_before_hint=2, show_survey=0):
+    def __init__(self, chinese, answer, hint='', error_count_before_hint=2, show_survey=0,
+                is_test=False, marker_data = ''):
         self.chinese = chinese
         self.answer = answer
         self.error_count_before_hint = error_count_before_hint
         self.hint = hint
         self.show_survey = show_survey
+        self.is_test = is_test
+        self.marker_data = marker_data
 
 
 SURVEY = [
@@ -52,9 +55,16 @@ PAGES = [
     Page('红', 'red'),
     Page('黄', 'yellow'),
     Page('红', 'red'),
-    Page('黄', 'yellow'),
     Page('红', 'red'),
-    Page('黄', 'yellow', show_survey=1),
+    Page('黄', 'yellow'),
+
+    Page('红', 'red', is_test = True, marker_data = 'easystart'),
+    Page('黄', 'yellow', is_test = True),
+    Page('红', 'red', is_test = True),
+    Page('红', 'red', is_test = True),
+    Page('黄', 'yellow', is_test = True),
+
+    Page('黄', 'yellow', is_test = True, show_survey=1, marker_data='easyend'),
 
     Page('黑', 'black', hint='黑 = black'),
     Page('橙', 'orange', hint='橙 = orange'),
@@ -65,7 +75,15 @@ PAGES = [
     Page('橙 绿 蓝', 'orange green blue'),
     Page('黑 红 粉', 'black red pink'),
     Page('紫 棕 蓝', 'purple brown blue'),
-    Page('绿 黄 白', 'green yellow white', show_survey=3),
+
+    Page('橙 绿 蓝', 'orange green blue', is_test=True, marker_data = 'hardstart'),
+    Page('黑 红 粉', 'black red pink', is_test=True),
+    Page('紫 棕 蓝', 'purple brown blue', is_test=True),
+    Page('橙 绿 蓝', 'orange green blue', is_test=True),
+    Page('黑 红 粉', 'black red pink', is_test=True),
+
+    Page('绿 黄 白', 'green yellow white', is_test=True, show_survey=3,
+    marker_data='hardend')
 ]
 
 '''Page('绿', 'green', hint='绿 = green'),
@@ -116,7 +134,7 @@ def question_start():
     time.sleep(3)
     sendData('baselineend')
     time.sleep(0.5)
-    sendData('easystart')
+    # sendData('easystart')
     return redirect(url_for('question', page=0))
 
 @app.route('/question/<int:page>', methods=['GET', 'POST'])
@@ -139,34 +157,43 @@ def question(page):
         user.put()'''
 
     # this is called after I press next on the survey
-    if canSendMarker:
-        if page == 10: # I will start with hard tasks
-            sendData('hardstart')
-            canSendMarker = False # prevent from sending markers on wrong answers
+    # if canSendMarker:
+    #     if page == 10: # I will start with hard tasks
+    #         sendData('hardstart')
+    #         canSendMarker = False # prevent from sending markers on wrong answers
 
     if page >= len(PAGES):
         session.pop('error_count')
         session.pop('uid')
         return render_template("cong.html")
 
+    # Adding condition for method!=POST so, we only send marker when page loads,
+    # not twice, for when page loads, and when the user submits an answer.
+    if PAGES[page].marker_data!='' and request.method!='POST':
+        sendData(PAGES[page].marker_data)
+
     if request.method == 'POST':
         expect = PAGES[page].answer
+
         if expect == '':
             return redirect(url_for('question', page=page + 1))
         got = request.form['answer']
         if got != expect:
-            session['error_count'][page_str] += 1
-            flash('Wrong!')
-            return redirect(url_for('question', page=page))
+            if PAGES[page].is_test:
+                return redirect(url_for('question', page=page+1))
+            else:
+                session['error_count'][page_str] += 1
+                flash('Wrong!')
+                return redirect(url_for('question', page=page))
         else: # means the answer is correct
-            canSendMarker = True
-            if canSendMarker:
-                if page == 9: # I finished the easy and will start survey so I will send easyend before starting the survey
-                    sendData('easyend')
-                    # I didn't change canSendMarker to false here because I need to send hard start when the new page 10 is loaded
-
-                if page == len(PAGES) - 1: # I finished the hard and will start survey so I will send hardend before starting the survey
-                    sendData('hardend')
+            # canSendMarker = True
+            # if canSendMarker:
+            #     if page == 9: # I finished the easy and will start survey so I will send easyend before starting the survey
+            #         sendData('easyend')
+            #         # I didn't change canSendMarker to false here because I need to send hard start when the new page 10 is loaded
+            #
+            #     if page == len(PAGES) - 1: # I finished the hard and will start survey so I will send hardend before starting the survey
+            #         sendData('hardend')
 
             if PAGES[page].show_survey != 0:
                 return redirect(url_for('survey', survey_num=PAGES[page].show_survey, next_page=page + 1))
