86d5a99eb9d755f73349ce567e45e3528a16718e
jin-sj <sj_jin@outlook.com>
2020-01-26 17:32:05 -0800

    cleanup

--- main.py
+++ main.py
@@ -2,20 +2,16 @@
 
 from __future__ import unicode_literals
 
-import collections
 import flask
-import logging
 import time
 import uuid
+import threading
 
-from datetime import datetime
 from pages import PAGES, Page
 from survey import SURVEY
+from simple_server import SocketServer
 
 from flask import flash, redirect, render_template, request, session, url_for
-from SocketClass import Socket_Class # import the socket class to send markers
-
-#from Data import User
 
 app = flask.Flask(__name__)
 
@@ -27,80 +23,58 @@ app.secret_key = 'dfasdfasdfasdf'
 
 trialNum = 0
 canSendMarker = False
-HOSTS = [("127.0.0.1", 8080), ("127.0.0.1", 8081)]
-SOCKETS = [None, None]
+sockett = None # socket variable created
+#SOCKET_SERVER_IP = "127.0.0.1"
+#SOCKET_SERVER_IP = "192.168.1.100"
+SOCKET_SERVER_IP = "0.0.0.0" # Might require change
+SOCKET_SERVER_PORT = 8080 # Might require change
+SERVER = None
 
 def send_data(class_label):
     global trialNum
     print("sending!")
 
     milliSec = int(round(time.time() * 1000))  # Get current time in milliseconds
-    data = "{};{};,{};\n".format(trialNum, class_label, milliSec)
-    for socket in SOCKETS:
-        socket.sendData(data)
+    data = "{};{};{};\n".format(trialNum, class_label, milliSec)
     trialNum += 1 # increment trial number
+    SERVER.broadcast(data)
 
+""" Runs the server in a separate thread
+    :param host: Server ip address
+    :param port: Server port
 """
-A function that opens a socket with Matlab PC
-"""
-def openSocket():
-    print("Waiting for connections")
-    connections = [False] * 2
-    try:
-        for i in range(len(connections)):
-            host, port = HOSTS[i][0], HOSTS[i][1]
-            socket = Socket_Class(host, port) # for dummy server
-            if socket:
-                SOCKETS[i] = socket
-                connections[i] = socket.openSocket()
-        return all(x for x in connections)
-    except Exception as e:
-        print(e)
-        return False
+def open_server(host, port):
+    global sockett
+    connected = openSocket()
+    if connected:
+        print("Connected sockett")
+    t1 = threading.Thread(target=run_server, args=(SOCKET_SERVER_IP, SOCKET_SERVER_PORT))
+    t1.start()
 
-'''
-A function that sends the marker's data through socket class and increment trial number
-'''
-#def sendData (classLabel):
-#    global trialNum
-#
-#    milliSec = int(round(time.time() * 1000))  # Get current time in milliseconds
-#    data = str(trialNum) +";"+classLabel+";" + str(milliSec) + ";\n"
-#    for socket in SOCKETS:
-#        socket.sendData(data)
-#    trialNum += 1 # increment trial number
+""" Initiate the server
+    :param host: Server ip address
+    :param port: Server port
+"""
+def run_server(host, port):
+    print("Opening server")
+    global SERVER
+    SERVER = SocketServer(SOCKET_SERVER_IP, SOCKET_SERVER_PORT)
+    SERVER.accept_connections()
 
 @app.route('/question/')
 def question_start():
-    #sendData('baselinestart')
     send_data('baselinestart')
     session['uid'] = uuid.uuid1()
     time.sleep(3)
-    #sendData('baselineend')
     send_data('baselineend')
     time.sleep(0.5)
-    #sendData('easystart')
-    send_Data('easystart')
+    send_data('easystart')
     return redirect(url_for('question', page=0))
 
 @app.route('/question/<int:page>', methods=['GET', 'POST'])
 def question(page):
     global canSendMarker
     page_str = str(page)
-
-    '''if page == 2:
-        user = User(name=str(session['uid']), pageId=page, time=str(datetime.now()), message='begin easy test')
-        user.put()
-
-    if page == 10:
-        user = User(name=str(session['uid']), pageId=page, time=str(datetime.now()),
-                    message='end survey 1, begin med test')
-        user.put()
-
-    if page == 25:
-        user = User(name=str(session['uid']), pageId=page, time=str(datetime.now()),
-                    message='end survey 2, begin hard test')
-        user.put()'''
 
     # this is called after I press next on the survey
     if canSendMarker:
@@ -127,12 +101,10 @@ def question(page):
             canSendMarker = True
             if canSendMarker:
                 if page == 9: # I finished the easy and will start survey so I will send easyend before starting the survey
-                    #sendData('easyend')
                     send_data('easyend')
                     # I didn't change canSendMarker to false here because I need to send hard start when the new page 10 is loaded
 
                 if page == len(PAGES) - 1: # I finished the hard and will start survey so I will send hardend before starting the survey
-                    #sendData('hardend')
                     send_data('hardend')
 
             if PAGES[page].show_survey != 0:
@@ -152,10 +124,8 @@ def question(page):
 
 @app.route('/')
 def hello_world():
-    if openSocket():
-        session.pop('error_count', None)
-        return render_template("welcomePage.html")
-
+    open_server(SOCKET_SERVER_IP, SOCKET_SERVER_PORT)
+    return render_template("welcomePage.html")
 
 @app.route('/survey/<int:survey_num>')
 def survey(survey_num):
@@ -167,10 +137,6 @@ def survey(survey_num):
         message = 'end med test, begin survey'
     if survey_num == 3:
         message = 'end hard test, begin survey'
-
-    '''user = User(name=str(session['uid']), pageId=int(next_page), time=str(datetime.now()),
-                message=message)
-    user.put()'''
 
     return render_template('survey.html', survey_num=survey_num, next_page=next_page, url=SURVEY[survey_num - 1])
 
