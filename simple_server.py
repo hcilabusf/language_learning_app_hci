03acea77d4be4f72cb0feaef1c2615205676db9d
alexziweiwang <zwang114@dons.usfca.edu>
2020-02-20 02:31:23 -0800

    file-wriring works with simple_client on tempfile

--- simple_server.py
+++ simple_server.py
@@ -2,6 +2,7 @@
 import socket
 from _thread import start_new_thread
 import threading
+import os
 
 class SocketServer:
     def __init__(self, ip, port):
@@ -15,16 +16,21 @@ class SocketServer:
     """ Thread for new client """
     def client_thread(self, conn, addr):
         # sends a message to the client whose user object is conn
-        #conn.send("Welcome to this chatroom!".encode())
+        
+        conn.send('Welcome to this chatroom!'.encode())
         while True:
             try:
-                message = conn.recv(2048)
+                message = conn.recv(2014).decode()
                 if message:
-                    print("<" + addr[0] + "> " + message)
 
                     # Calls broadcast function to send message to all
-                    message_to_send = "<" + addr[0] + "> " + message
-                    #self.broadcast(message_to_send, conn)
+                    message_to_send = "<" + addr[0] + "> " + message        
+                    print('msg to send: [' + message_to_send + ']')
+                    with open("tempfile", "a") as myfile:
+                        myfile.write(message_to_send)
+                        myfile.write('\n')            
+#                    self.broadcast(message_to_send)
+                    f.close()          
                 else:
                     self.remove(conn)
                     break
@@ -33,17 +39,18 @@ class SocketServer:
                 continue
 
 
-    """ Broadcast a message to all connected clients"""
-    def broadcast(self, message):
-        #print("ALL connections: %s" % self.list_of_clients)
-        print("Number of connections: %s" % len(self.list_of_clients))
-        for clients in self.list_of_clients:
-            try:
-                print("Sending %s to: %s" % (message, clients))
-                clients.send(message.encode())
-            except Exception:
-                clients.close()
-                self.remove(clients)
+#    """ Broadcast a message to all connected clients"""
+#    def broadcast(self, message):
+#        #print("ALL connections: %s" % self.list_of_clients)
+#        print("\nBroadcast: Number of connections: %s" % len(self.list_of_clients))
+#        for clients in self.list_of_clients:
+#            try:
+#                print("Sending %s to: %s" % (message, clients))
+#                clients.send(message.encode())
+#            except Exception:
+#                clients.close()
+#                self.remove(clients)
+
 
     """ Remove a sockets connection """
     def remove(self, connection):
@@ -53,6 +60,7 @@ class SocketServer:
     """ Accept new socketes connections """
     def accept_connections(self):
         while True:
+            print('...in the connecting- start of true while loop')
             conn, addr = self.server.accept()
 
             self.list_of_clients.append(conn)
@@ -61,11 +69,15 @@ class SocketServer:
             print(addr[0] + " connected")
 
             # creates and individual thread for every user that connects
-            threading.Thread(target=self.client_thread, args=((conn, addr)))
+            process = threading.Thread(target=self.client_thread, args=[conn, addr])
+            process.start()
+
 
 def main():
-    server = SocketServer("0.0.0.0", 5000)
+    server = SocketServer("127.0.0.1", 8080)
     server.accept_connections()
+    print('end of main')
+
 
 if __name__ == "__main__":
     main()
